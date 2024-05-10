c8cccdef2c7d4382bf76392affd88246e91a8fdc
CSneko <minecrafthetun@outlook.com>
2024-05-10 22:03:08 +0800

    init

---
+++ src/main/java/org/cneko/strange/StrangeCreatures.java
@@ -0,0 +1,10 @@
+package org.cneko.strange;
+
+import net.fabricmc.api.ModInitializer;
+
+public class StrangeCreatures implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/org/cneko/strange/client/StrangeCreaturesClient.java
@@ -0,0 +1,10 @@
+package org.cneko.strange.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StrangeCreaturesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
